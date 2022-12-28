91e47b56fd27785ae2042bdef94bb4572b98a1ce
MohamedAbdElNaby <31933643+MohamedAbdElNaby@users.noreply.github.com>
2022-12-28 21:11:53 +0200

    fix/for ios

--- src/components/bottomSheet/BottomSheet.tsx
+++ src/components/bottomSheet/BottomSheet.tsx
@@ -308,11 +308,7 @@ const BottomSheetComponent = forwardRef<BottomSheet, BottomSheetProps>(
             animatedKeyboardHeight.value -
               Math.abs(bottomInset - animatedContainerOffset.value.bottom)
           )
-        :Platform.OS=='ios'?
-        Math.abs(
-            animatedKeyboardHeight.value - animatedContainerOffset.value.bottom
-          )
-          : 5;
+        : 5;
     }, [$modal, bottomInset]);
     //#endregion
 
