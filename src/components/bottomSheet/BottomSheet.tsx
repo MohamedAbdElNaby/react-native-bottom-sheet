ec333e332993ae295bfd5717da1ae2e2840db970
MohamedAbdElNaby <31933643+MohamedAbdElNaby@users.noreply.github.com>
2023-01-09 10:53:42 +0200

    fix keyboard

--- src/components/bottomSheet/BottomSheet.tsx
+++ src/components/bottomSheet/BottomSheet.tsx
@@ -308,7 +308,7 @@ const BottomSheetComponent = forwardRef<BottomSheet, BottomSheetProps>(
             animatedKeyboardHeight.value -
               Math.abs(bottomInset - animatedContainerOffset.value.bottom)
           )
-        :Platform.OS === 'android' ? 5: Math.abs(
+        :props.currentKeyboardHieght|| Math.abs(
              animatedKeyboardHeight.value - animatedContainerOffset.value.bottom
            );
     }, [$modal, bottomInset]);
